e2b2f728c45a36a33d230a1ff8f77e698c224ead
Vikas Chaurasia <vikascc28@gmail.com>
2024-04-06 23:12:13 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">The Institute</a></li>
+        <li><a href="/">Academics</a></li>
+        <li><a href="/">Administration</a></li>
+        <li><a href="/">Departments</a></li>
+        <li><a href="/">Facilities</a></li>
+        <li><a href='/'>Placements</a></li>
+        <li><a href="/">Student Zone</a></li>
+      </ul>
+    </nav>
   )
 }
 
