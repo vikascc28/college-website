b522a36ef991d8928e2f923b841bbc09d8da7b55
Vikas Chaurasia <vikascc28@gmail.com>
2024-05-12 22:07:00 +0530

    add Campus Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 
 const App = () => {
@@ -11,8 +14,13 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
-        <Title subTitle='Our Programs' title='What We Offer'/>
+        <Title subTitle='Our Programs' title='What We Offer' />
         <Programs />
+        <About />
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus />
+        <Title subTitle='Testimonials' title='What Student Says' />
+        <Testimonials />
       </div>
     </div>
   )
